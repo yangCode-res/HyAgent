8628f33c8325170ba3df89d73bf4b40deeca9e5e
yang <1378055043@qq.com>
2025-11-04 15:49:21 +0800

    memory

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from Agents.Entity_extraction.index import main
 from ChatLLM.index import ChatLLM
 from Logger.index import get_global_logger
-from Memory.index import memory 
-memory.dump_json("./snapshots")
+from Store.index import get_memory
 if __name__ == "__main__":
+    memory = get_memory()
+    
     logger = get_global_logger()
     logger.info("Starting HyGraph...")
     main()
     logger.info("HyGraph finished.")
     logger.info("="*100)
+    memory.dump_json("./snapshots")
