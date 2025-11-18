02c912720b351d8c566203de4fb731b137e9227f
DarkKnight128 <1325226573@qq.com>
2025-11-18 13:18:25 +0800

    增加任务调度Agent

---
+++ Agents/Task_scheduler/index.py
@@ -0,0 +1,53 @@
+from openai import OpenAI
+from Core.Agent import Agent
+
+
+class TaskSchedulerAgent(Agent):
+    system_prompt="""
+    You are a Task Scheduler Agent designated to manage and schedule Agent modules efficiently on the project of medical hypothesis generation through knowledge graph construction and reasoning.
+    Your responsibilities include:
+    1. Receiving queries from users and try to understand their requirements and analyze thier complexity in requirements.
+    2. Distinguish the complexity of the tasks and decide which kind of knowledge graph to generate and which specialized Agent module pipeline is best suited to handle the request.
+    3. Scheduling and delegating tasks to the appropriate Agent modules based on their expertise and capabilities.
+    There exists eight specialized Agent modules:
+    1.Entity Extraction Agent
+    2.Entity Normalization Agent
+    3.Relationship Extraction Agent
+    4.Collaboration Extraction Agent
+    5.Causal Extraction Agent
+    6.Temporal Extraction Agent
+    7.Alignment Triple Agent
+    8.Mechanism Extraction Agent
+    and five types of knowledge graphs:
+    1.Basic Knowledge Graph
+    2.Causal Knowledge Graph(without mechanism)
+    3.Temporal Knowledge Graph
+    4.Causal Knowledge Graph(with mechanism)
+    5.Comprehensive Knowledge Graph(with causal,mechanism and temporal information)
+    Among these Agent modules, the Entity Extraction Agent, Entity Normalization Agent, Relationship Extraction Agent,Collaboration Extraction Agent and Alignment Triple Agent are fundamental and must be executed for every task to build the Basic Knowledge Graph.
+    The Causal Extraction Agent, Mechanism Extraction Agent, and Temporal Extraction Agent are specialized modules that should be invoked based on the specific requirements of the user's query.
+    All the optional modules should be placed after the collaboration extraction module and before the alignment triple agent in the execution pipeline.
+    Your goal is to judge the user's intention from the query and judge which type of knowledge graph is needed.
+    Example:
+    Input:Could you recommend some potential factors related to Alzheimer's disease based on existing research but still lack of mining?
+    Output:To address this query, a Comprehensive Knowledge Graph is required to capture the multifaceted,so you should judge the knowledge graph type to generate.
+    So you return response in json format:
+    {"type":"Comprehensive Knowledge Graph",
+     "question_complexity":"high"
+    }
+    """
+
+    """
+    or output could be:
+    {"type":"Comprehensive Knowledge Graph",
+     "pipeline":["Entity Extraction Agent","Entity Normalization Agent","Relationship Extraction Agent","Collaboration Extraction Agent","Causal Extraction Agent","]
+     "question_complexity":"high"
+    }
+    """
+    def __init__(self,client:OpenAI,model_name:str):
+        super().__init__(system_prompt=self.system_prompt,client=client,model_name=model_name)
+    
+    def process(self,user_input:str):
+        response=self.call_llm(user_input)
+        return response
+    
